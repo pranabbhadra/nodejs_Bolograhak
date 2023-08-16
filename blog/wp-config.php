1d383b3bb92e5e9832fd68e598bd9b02b335e553
Debashish Roy <debashishroy@Debashishs-MBP.Dlink>
2023-08-16 21:30:10 +0530

    work done on 16082023

--- blog/wp-config.php
+++ blog/wp-config.php
@@ -50,19 +50,20 @@ define( 'DB_NAME', 'bolograhak' );
 
 /** Database username */
 
-define( 'DB_USER', 'root' );
+define( 'DB_USER', 'admin' );
 
 
 
 /** Database password */
 
-define( 'DB_PASSWORD', '' );
+define( 'DB_PASSWORD', 'bGdbPa55w0rD' );
 
 
 
 /** Database hostname */
 
-define( 'DB_HOST', 'localhost' );
+define( 'DB_HOST', 'bolograhakdb.cb41jdmx4rqo.us-east-1.rds.amazonaws.com' );
+
 
 
 
