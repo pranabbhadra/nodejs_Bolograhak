a69b6e73bee33320eecec33fc5b39f932d038a8d
Vivek Roy <vivek@scwebtech.com>
2023-08-26 18:31:11 +0530

    User profile API Update 15

--- common_function_api.js
+++ common_function_api.js
@@ -886,7 +886,7 @@ function getUserCompany(user_ID){
           if (err) {
             reject(err);
           } else {
-            resolve(result);
+            resolve(user_ID);
           }
         });
     });
