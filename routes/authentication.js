a2bd680152062feb2836bb22f774e39874d67278
Vivek Roy <vivek@scwebtech.com>
2023-08-26 13:05:14 +0530

    API work JWT Add function 26-8-23 7

--- routes/authentication.js
+++ routes/authentication.js
@@ -26,7 +26,68 @@ const storage = multer.diskStorage({
 // Create multer instance
 const upload = multer({ storage: storage });
 
+//get user details
+router.get('/getUserDetails/:user_id', async (req, res) => {
+    const { user_id } = req.params;
+    console.log(user_id);
 
+    const query = 'SELECT u.user_id, u.first_name, u.last_name, u.email, u.phone, u.user_registered, u.register_from, u.external_registration_id, u.user_type_id, u.user_status, m.address, m.country, m.state, m.city, m.zip, m.date_of_birth, m.occupation, m.gender, m.profile_pic, m.alternate_phone, m.marital_status,m.about FROM users u LEFT JOIN user_customer_meta m on u.user_id=m.user_id WHERE u.user_id=?';
+
+    db.query(query, [user_id], (err, results) => {
+        if (err) {
+            return res.status(500).json({
+                status: 'error',
+                message: 'An error occurred while fetching user details',
+            });
+        }
+
+        if (results.length === 0) {
+            return res.status(404).json({
+                status: 'error',
+                message: 'User not found',
+            });
+        }
+
+        const userDetails = results[0];
+        const countryQuery = 'SELECT name FROM countries WHERE id=?';
+        db.query(countryQuery, [userDetails.country], (countryErr, countryResults) => {
+            if (countryErr) {
+                return res.status(500).json({
+                    status: 'error',
+                    message: 'An error occurred while fetching country details',
+                });
+            }
+
+            if (Array.isArray(countryResults) && countryResults.length > 0) {
+                userDetails.countryname = countryResults[0].name;
+            } else {
+                userDetails.countryname = 'Unknown Country';
+            }
+
+            const stateQuery = 'SELECT name FROM states WHERE id=?';
+            db.query(stateQuery, [userDetails.state], (stateErr, stateResults) => {
+                if (stateErr) {
+                    return res.status(500).json({
+                        status: 'error',
+                        message: 'An error occurred while fetching state details',
+                    });
+                }
+
+                if (Array.isArray(stateResults) && stateResults.length > 0) {
+                    userDetails.statename = stateResults[0].name;
+                } else {
+                    userDetails.statename = 'Unknown State';
+                }
+
+                return res.status(200).json({
+                    status: 'success',
+                    data: userDetails,
+                    message: 'User details fetched successfully',
+                });
+            });
+        });
+    });
+});
 
 router.post('/register',upload.single('profile_pic') ,authenController.register);
 router.post('/login', authenController.login);
