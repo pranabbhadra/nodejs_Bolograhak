fd1236636694d630813b11da4e27dd07501606e3
Vivek Roy <vivek@scwebtech.com>
2023-08-26 13:19:08 +0530

    API work JWT Add function 26-8-23 10

--- routes/authentication.js
+++ routes/authentication.js
@@ -9,6 +9,34 @@ const db = require('../config');
 const router = express.Router();
 //const publicPath = path.join(__dirname,'../public');
 
+
+
+// Set up multer storage for file upload
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'uploads/');
+    },
+    filename: function (req, file, cb) {
+        // const originalname = file.originalname;
+        // const sanitizedFilename = originalname.replace(/[^a-zA-Z0-9\-\_\.]/g, ''); // Remove symbols and spaces
+        // const filename = Date.now() + '-' + sanitizedFilename;
+        // cb(null, filename);
+        const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
+        cb(null, file.fieldname + '-' + uniqueSuffix);
+    }
+});
+// Create multer instance
+const upload = multer({ storage: storage });
+
+router.post('/register',upload.single('profile_pic') ,authenController.register);
+router.post('/login', authenController.login);
+router.put('/edituser', verifyToken, upload.single('profile_pic') ,authenController.edituser);
+router.post('/createcategories',verifyToken, upload.single('c_image'),authenController.createcategories);
+router.post('/createcompany',verifyToken, upload.single('logo') ,authenController.createcompany);
+router.put('/editcompany',verifyToken, upload.single('logo') ,authenController.editcompany);
+router.post('/createcompanylocation',verifyToken, authenController.createcompanylocation);
+router.post('/submitReview',verifyToken, authenController.submitReview);
+
 //get user details
 router.get('/getUserDetails/:user_id', async (req, res) => {
     const { user_id } = req.params;
@@ -71,32 +99,6 @@ router.get('/getUserDetails/:user_id', async (req, res) => {
         });
     });
 });
-
-// Set up multer storage for file upload
-const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, 'uploads/');
-    },
-    filename: function (req, file, cb) {
-        // const originalname = file.originalname;
-        // const sanitizedFilename = originalname.replace(/[^a-zA-Z0-9\-\_\.]/g, ''); // Remove symbols and spaces
-        // const filename = Date.now() + '-' + sanitizedFilename;
-        // cb(null, filename);
-        const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
-        cb(null, file.fieldname + '-' + uniqueSuffix);
-    }
-});
-// Create multer instance
-const upload = multer({ storage: storage });
-
-router.post('/register',upload.single('profile_pic') ,authenController.register);
-router.post('/login', authenController.login);
-router.put('/edituser', verifyToken, upload.single('profile_pic') ,authenController.edituser);
-router.post('/createcategories',verifyToken, upload.single('c_image'),authenController.createcategories);
-router.post('/createcompany',verifyToken, upload.single('logo') ,authenController.createcompany);
-router.put('/editcompany',verifyToken, upload.single('logo') ,authenController.editcompany);
-router.post('/createcompanylocation',verifyToken, authenController.createcompanylocation);
-router.post('/submitReview',verifyToken, authenController.submitReview);
 
 function verifyToken(req, res, next){
     let token = req.headers['authorization'];
