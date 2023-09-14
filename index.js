dd588016ca3a7d79540d253368de560d7f8a67b8
Vivek Roy <vivek@scwebtech.com>
2023-09-14 18:57:54 +0530

    FB login code update

--- index.js
+++ index.js
@@ -170,14 +170,8 @@ app.get('/facebook-user-data', async(req, res) => {
                 const external_id = UserResponse.external_registration_id;
 
                 const currentDate = new Date();
-                const year = currentDate.getFullYear();
-                const month = String(currentDate.getMonth() + 1).padStart(2, '0');
-                const day = String(currentDate.getDate()).padStart(2, '0');
-                const hours = String(currentDate.getHours()).padStart(2, '0');
-                const minutes = String(currentDate.getMinutes()).padStart(2, '0');
-                const seconds = String(currentDate.getSeconds()).padStart(2, '0');
 
-                const formattedDate = `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
+                const formattedDate = currentDate.toISOString().slice(0, 19).replace('T', ' ');
 
                 let hasPassword = await bcrypt.hash(userEmail, 8);
 
