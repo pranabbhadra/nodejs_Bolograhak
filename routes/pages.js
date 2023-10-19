bb3ff90fd7f40023d435482d4b75e856321c3bca
debalina <debalina@scwebtech.com>
2023-10-19 12:45:22 +0530

    complain page working

--- routes/pages.js
+++ routes/pages.js
@@ -1167,7 +1167,7 @@ router.get('/basic-create-category', checkCookieValue, async (req, res) => {
     //res.render('front-end/terms-of-service', { menu_active_id: 'terms-of-service', page_title: 'Terms Of Service', currentUserData });
 });
 
-//premium company-compalin-details
+//premium company-complain-details
 router.get('/premium-company-complain-details', checkCookieValue, async (req, res) => {
     let currentUserData = JSON.parse(req.userData);
     const [globalPageMeta] = await Promise.all([
@@ -1188,7 +1188,7 @@ router.get('/premium-company-complain-details', checkCookieValue, async (req, re
     //res.render('front-end/terms-of-service', { menu_active_id: 'terms-of-service', page_title: 'Terms Of Service', currentUserData });
 });
 
-//premium company-compalin-details
+//basic company-complain-details
 router.get('/basic-company-complain-details', checkCookieValue, async (req, res) => {
     let currentUserData = JSON.parse(req.userData);
     const [globalPageMeta] = await Promise.all([
@@ -1199,6 +1199,27 @@ router.get('/basic-company-complain-details', checkCookieValue, async (req, res)
         res.render('front-end/basic-company-complain-details', {
             menu_active_id: 'basic-company-complain-details',
             page_title: 'Company Complain',
+            currentUserData,
+            globalPageMeta:globalPageMeta
+        });
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('An error occurred');
+    }
+    //res.render('front-end/terms-of-service', { menu_active_id: 'terms-of-service', page_title: 'Terms Of Service', currentUserData });
+});
+
+//premium customer-complain-details
+router.get('/premium-customer-complain-details', checkCookieValue, async (req, res) => {
+    let currentUserData = JSON.parse(req.userData);
+    const [globalPageMeta] = await Promise.all([
+        comFunction2.getPageMetaValues('global'),
+    ]);
+    try {
+
+        res.render('front-end/premium-customer-complain-details', {
+            menu_active_id: 'premium-customer-complain-details',
+            page_title: 'Customer Complain',
             currentUserData,
             globalPageMeta:globalPageMeta
         });
