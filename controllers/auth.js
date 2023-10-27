b81cc8d7685799a0470a34b5218b7f58a9315e33
Vivek Roy <vivek@scwebtech.com>
2023-10-27 18:10:46 +0530

    login with social coding update

--- controllers/auth.js
+++ controllers/auth.js
@@ -142,16 +142,16 @@ exports.frontendUserRegister = async (req, res) => {
             });
         });
         if (emailExists) {
-            let register_from = results[0].register_from;
-            if(register_from=='web'){
+            // let register_from = results[0].register_from;
+            // if(register_from=='web'){
                 var message = 'Email ID already exists, Please login with your email-ID and password';
-            }else{
-                var message = 'Email ID already exists, login with '+register_from;
-            }
+            // }else{
+            //     var message = 'Email ID already exists, login with '+register_from;
+            // }
             return res.send(
                 {
                     status: 'err',
-                    data: '',
+                    data: results,
                     message: message
                 }
             )
