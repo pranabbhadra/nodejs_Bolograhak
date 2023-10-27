d45432e0feb6b219c3e4bf295c0a52bae54476c9
Vivek Roy <vivek@scwebtech.com>
2023-10-27 17:15:34 +0530

    login with social coding update

--- index.js
+++ index.js
@@ -368,7 +368,7 @@ app.get('/google-user-data', async(req, res) => {
 
         }else{
             if(UserResponse.register_from=='web'){
-                alert('This email-ID already exist please login with your email and password.');
+                //alert('This email-ID already exist please login with your email and password.');
                 return res.redirect('/error/'+UserResponse.register_from);
             }else{
                 //login code gose here
@@ -566,7 +566,7 @@ app.get('/google-user-data', async(req, res) => {
         }            
     } catch (error) {
         console.error('Error saving user data:', error);
-        return res.redirect('/error/'+error.message);
+        return res.redirect('/error2/'+error.message);
     }    
 });
 
