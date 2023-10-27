8a2f493577f55cad4cd8b3d9409c532068818ab0
Vivek Roy <vivek@scwebtech.com>
2023-10-27 18:08:14 +0530

    login with social coding update

--- controllers/auth.js
+++ controllers/auth.js
@@ -144,9 +144,9 @@ exports.frontendUserRegister = async (req, res) => {
         if (emailExists) {
             let register_from = results[0].register_from;
             if(register_from=='web'){
-                let message = 'Email ID already exists, Please login with your email-ID and password';
+                var message = 'Email ID already exists, Please login with your email-ID and password';
             }else{
-                let message = 'Email ID already exists, login with '+register_from;
+                var message = 'Email ID already exists, login with '+register_from;
             }
             return res.send(
                 {
